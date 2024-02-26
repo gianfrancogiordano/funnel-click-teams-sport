5f83eca148f8870598f3bdeb1dbbcd692de9d987
Gianfrano Giordano <gianfranogiordano@gmail.com>
2024-02-26 06:45:08 -0500

    Primer commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  base_url: 'https://api.soyclickstore.com/api/v1',
+  wsUrl: 'https://api.soyclickstore.com',
+  cloudFront: 'https://d3cj1y2eynyat2.cloudfront.net',
+  clickstoreId: '65c0f635f133f902a7d814b7',
+  clicstoreDomain: 'https://teamssport.getcs.link',
+  pixel: '817317110157671',
+  apiFB: 'EAAKo6KJmMZAkBO4UW68ZCeUZCUoWCze7zhRfGRcmZBiMgPFN6YTxREUY1OrB9LrAwQAjvC6jYk4btYoZAOVXqj7pl0qpGERAWfrOZALjjx3GpsbddZAFHcsbJo9Ap04EgcBx3b9qAMnBn2tVdD8NN776CxLUWcMxRe4qtYX0FCdzdQdSlOm4ggXdsa2xg4pMJ4MIQZDZD'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  base_url: 'https://api.soyclickstore.com/api/v1',
+  wsUrl: 'https://api.soyclickstore.com',
+  cloudFront: 'https://d3cj1y2eynyat2.cloudfront.net',
+  clickstoreId: '65c0f635f133f902a7d814b7',
+  clicstoreDomain: 'https://teamssport.getcs.link',
+  pixel: '817317110157671',
+  apiFB: 'EAAKo6KJmMZAkBO4UW68ZCeUZCUoWCze7zhRfGRcmZBiMgPFN6YTxREUY1OrB9LrAwQAjvC6jYk4btYoZAOVXqj7pl0qpGERAWfrOZALjjx3GpsbddZAFHcsbJo9Ap04EgcBx3b9qAMnBn2tVdD8NN776CxLUWcMxRe4qtYX0FCdzdQdSlOm4ggXdsa2xg4pMJ4MIQZDZD'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
